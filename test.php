f14f3b3d10642acc9c7b5ad23cae786d7a66b5db
bekasovme <bekasovme@gmail.com>
2018-03-29 17:15:42 +0300

    четвертое изменение

--- test.php
+++ test.php
@@ -1,9 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		echo "Hello World!<br>";
+		echo "Тест версии<br>";
+		$name = "Michael";
+	?>
 
-	echo "Hello World!";<br>
-	echo "Тест версии";<br>
-
-	$name = "Michael";
-?>
-
-<?= $name ?>
+	<?= $name ?>
+</body>
+</html>
