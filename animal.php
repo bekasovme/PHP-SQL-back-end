3aff90aa58877ec0058d85dcbac9abf5ce41cc79
bekasovme <bekasovme@gmail.com>
2018-04-03 19:48:17 +0300

    lesson_1.4

--- animal.php
+++ animal.php
@@ -1,12 +1,29 @@
 <?php
 
 $continents = array(
-	// 'africa' => 'Mammuthus columbi'
-	'John',
-	'Mary',
-	'Walker'
+	'africa' => array(
+		'macaca sylvanus',
+		'panthera pardus',
+		'felis margarita',
+		'gazella dorcas',
+		'hippopotamus',
+		'theropithecus gelada',
+		'psittacus erithacus',
+		'trioceros jacksonii',
+		'pan paniscus',
+		'colobus',
+		'hippotigris',
+		'tragelaphus imberbis',
+		'giraffa'
+	)
+	// 'eurasia' =>
+	// 'north America' =>
+	// 'south America' =>
+	// 'australia' =>
+	// 'antarctica' =>
+
 );
 
-echo $continents[0];
+echo $continents['africa'][6];
 
 ?>
