c631e3d55709fffc066e507b970ab712b6e76594
bekasovme <bekasovme@gmail.com>
2018-04-03 20:34:12 +0300

    lesson_1.4

--- animal.php
+++ animal.php
@@ -7,14 +7,30 @@ $continents = array(
 		'felis margarita'
 	)
 	'eurasia' => array(
+		'capra',
+		'meleseles meles',
+		'tarsius'
+
 	)
 	'north America' => array(
+		'Rangifer tarandus',
+		'Lynx rufus',
+		'Canis latrans'
 	)
 	'south America' => array(
+		'Vicugna vicugna',
+		'Hydrochoerus hydrochaeris',
+		'Panthera onca'
 	)
 	'australia' => array(
+		'Canis lupus dingo',
+		'Macropodidae',
+		'Raphus cucullatus'
 	)
 	'antarctica' => array(
+		'Ursus maritimus',
+		'Aptenodytes patagonicus',
+		'Nyctea scandiaca'
 	)
 
 );
