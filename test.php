1efeec37e70b8279f2ded7d19dfab3b967dd945c
bekasovme <bekasovme@gmail.com>
2018-04-09 19:29:29 +0300

    lesson_1.5_test

--- test.php
+++ test.php
@@ -1,16 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Document</title>
-</head>
-<body>
-	<?php
-		echo "Hello World!<br>";
-		echo "Тест версии<br>";
-		$name = "Michael";
-	?>
+<?php
 
-	<?= $name ?>
-</body>
-</html>
+// 524901  id Москвы
+
+$url = "http://api.openweathermap.org/data/2.5/weather?id=524901&lang=en&units=metric&APPID=твой id";
+
+
+$contents = file_get_contents($url);
+$clima=json_decode($contents);
+
+$temp_max=$clima->main->temp_max;
+$temp_min=$clima->main->temp_min;
+$icon=$clima->weather[0]->icon.".png";
+//how get today date time PHP :P
+$today = date("F j, Y, g:i a");
+$cityname = $clima->name; 
+
+echo $cityname . " - " .$today . "<br>";
+echo "Temp Max: " . $temp_max ."&deg;C<br>";
+echo "Temp Min: " . $temp_min ."&deg;C<br>";
+echo "<img src='http://openweathermap.org/img/w/" . $icon ."'/ >";
+
+?>
