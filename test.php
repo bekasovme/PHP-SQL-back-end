e26ae446c3bc44b0f4f39a7ebdb59fe56988c965
bekasovme <bekasovme@gmail.com>
2018-04-09 23:12:15 +0300

    lesson_1.5

--- test.php
+++ test.php
@@ -1,23 +1,12 @@
+
 <?php
-
-// 524901  id Москвы
-
-$url = "http://api.openweathermap.org/data/2.5/weather?id=524901&lang=en&units=metric&APPID=твой id";
-
-
-$contents = file_get_contents($url);
-$clima=json_decode($contents);
-
-$temp_max=$clima->main->temp_max;
-$temp_min=$clima->main->temp_min;
-$icon=$clima->weather[0]->icon.".png";
-//how get today date time PHP :P
-$today = date("F j, Y, g:i a");
-$cityname = $clima->name; 
-
-echo $cityname . " - " .$today . "<br>";
-echo "Temp Max: " . $temp_max ."&deg;C<br>";
-echo "Temp Min: " . $temp_min ."&deg;C<br>";
-echo "<img src='http://openweathermap.org/img/w/" . $icon ."'/ >";
-
+ 
+$url="http://api.openweathermap.org/data/2.5/forecast?q=London,usl&appid=2de143494c0b295cca9337e1e96b00e0;units=metric&cnt=7&lang=en";
+$json=file_get_contents($url);
+$data=json_decode($json,true);
+echo "Temp: ". $data['main']['temp']."ºC";
+echo $data['weather'][0]['description']."";
+echo $data['weather'][0]['main']."";
+echo "Wind Speed: " . $data['wind']['speed']." mph";
+ 
 ?>
